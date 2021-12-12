ae44857a0968d13f2bbff2d92cb977c522821368
Serhii Hult <gyltsergiy@gmail.com>
2021-12-12 21:32:31 +0200

    added animation for img

--- src/javascript/weatherAPI.js
+++ src/javascript/weatherAPI.js
@@ -2,20 +2,19 @@ let text = document.querySelector('.temperature');
 
 console.log(text);
 
-function fetchWeather() {
-	//use the values from latitude and longitude to fetch the weather
+async function fetchWeather() {
 	let key = 'dd47af45cbc9862e2129c021a8f27e21';
 	let city = 'Kyiv';
 	let url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 	//fetch the weather
-	fetch(url)
-		.then(res => {
-			if (!res.ok) throw new Error(res.statusText);
-			return res.json();
+	const response = await fetch(url)
+		.then(response => {
+			return response.json();
 		})
 		.then(data => {
 			console.log(data);
-		})
-		.catch(console.err);
+		});
+	return await response.json();
 }
+
 fetchWeather();
